775fb9d23962e1f7c849dec4b59e5f6d98005eee
vinimou <viniciusmoutinho.vm@gmail.com>
2024-05-15 20:04:56 -0300

    Exercicios feitos 15/05

---
+++ exercicios/src/fundamentos/PrimitivosVsObjeto.java
@@ -0,0 +1,16 @@
+package fundamentos;
+
+public class PrimitivosVsObjeto {
+	public static void main(String[] args) {
+		
+		
+		String s = "texto"; 
+		s.toUpperCase();
+		
+		
+		//Wrappers é a versão objeto dos tipos primitivos 
+		int a = 123;
+		System.out.println(a);
+	}
+
+}
